5ee2a518b266619ad581b54e21b448dbce8ac945
rankangan <rankangan@wps.cn>
2023-08-17 15:21:37 +0800

    feat: update

--- lib/src/core.ts
+++ lib/src/core.ts
@@ -1,10 +1,11 @@
-export type Subscriber = (l: Function) => () => void
+export type Listener<T> = (state: T, prevState: T) => void
+export type Subscribe<T> = (l: Listener<T>) => () => void
 export type SetState<T> = (partial: (state: T) => Partial<T> | Partial<T>, replace?: boolean) => void
 export type GetState<T> = () => T
 export type StoreApi<T> = {
   setState: SetState<T>
   getState: GetState<T>
-  subscribe: Subscriber
+  subscribe: Subscribe<T>
 }
 export type CreateState<T> = (set: SetState<T>, get: GetState<T>, api: StoreApi<T>) => T
 
@@ -12,7 +13,7 @@ export type CreateState<T> = (set: SetState<T>, get: GetState<T>, api: StoreApi<
 export function create<T>(createState: CreateState<T>): StoreApi<T> {
   // 创建 state
   let state: T
-  const listeners = new Set<Function>()
+  const listeners = new Set<Listener<T>>()
 
   // 创建 getState
   const getState = () => state
@@ -24,12 +25,12 @@ export function create<T>(createState: CreateState<T>): StoreApi<T> {
     if (!Object.is(prevState, nextState)) {
       // 浅比较
       state = (replace ?? typeof nextState !== 'object') ? nextState as T : Object.assign({}, state, nextState)
-      listeners.forEach((listener) => listener())
+      listeners.forEach((listener) => listener(state, prevState))
     }
   }
 
   // 创建 subscribe
-  const subscribe = (listener: Function) => {
+  const subscribe = (listener: Listener<T>) => {
     listeners.add(listener)
     return () => {
       listeners.delete(listener)
